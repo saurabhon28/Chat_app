a37c8e79b00eb683b7820c8bc3040b2cc79d2947
saurabhon28 <kumar.saurabh906@gmail.com>
2023-10-12 12:04:46 +0530

    seperate style from pages file

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
-import styled from "styled-components";
+import { Container } from "./styleChat";
 import { allUsersRoute, host } from "../utils/APIRoutes";
 import ChatContainer from "../components/ChatContainer";
 import Contacts from "../components/Contacts";
@@ -29,6 +29,7 @@ export default function Chat() {
     }
     fetchData();
   }, []);
+ 
   useEffect(() => {
     if (currentUser) {
       socket.current = io(host);
@@ -68,23 +69,3 @@ export default function Chat() {
   );
 }
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1rem;
-  align-items: center;
-  background-color: #131324;
-  .container {
-    height: 85vh;
-    width: 85vw;
-    background-color: #00000076;
-    display: grid;
-    grid-template-columns: 25% 75%;
-    @media screen and (min-width: 720px) and (max-width: 1080px) {
-      grid-template-columns: 35% 65%;
-    }
-  }
-`;
