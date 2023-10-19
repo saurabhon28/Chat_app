cd1dd59926a156755783d75418b7fc08482f03d8
saurabhon28 <kumar.saurabh906@gmail.com>
2023-10-19 13:09:17 +0530

    set the client url with socket io

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const server = app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000" || "https://chat-app-client-mu-eight.vercel.app/",
     credentials: true,
   },
 });
